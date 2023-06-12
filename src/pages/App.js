fd116b8456f37f91326f0b7cbdb83db1490b7885
zayq <jasmin.robert07@gmail.com>
2023-06-12 16:05:37 -0400

    gfd

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,7 @@ import { Download } from '../api/download';
 import "../App.css"
 import io from "socket.io-client"
 
-const socket = io('http://localhost:1234/');
+const socket = io('https://serveryouzay.zaylab.com/');
 
 
 function App() {
